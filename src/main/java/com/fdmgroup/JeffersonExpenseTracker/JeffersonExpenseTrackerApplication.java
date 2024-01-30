fe63202f79b09a0a9c7efac7c30eff17a9b4ca59
Jefferson <68528609+jefferson6263@users.noreply.github.com>
2024-01-30 11:36:24 +1100

    Initial commit

---
+++ src/main/java/com/fdmgroup/JeffersonExpenseTracker/JeffersonExpenseTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.fdmgroup.JeffersonExpenseTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeffersonExpenseTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JeffersonExpenseTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fdmgroup/JeffersonExpenseTracker/JeffersonExpenseTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fdmgroup.JeffersonExpenseTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JeffersonExpenseTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
